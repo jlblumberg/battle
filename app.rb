0a9ef16a83c8c9c1d3a3ac6cc1812294c5835e14
Josh Blumberg <jlblumberg@gmail.com>
2020-02-05 14:15:06 +0000

    Add functionality for entering names

--- app.rb
+++ app.rb
@@ -1,8 +1,15 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  
   get '/' do
-    "Testing infrastructure working!"
+    erb(:index)
+  end
+
+  post '/names' do
+    @player_1 = params[:player_1]
+    @player_2 = params[:player_2]
+    erb(:play)
   end
 
   # start the server if ruby file executed directly
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,18 +1,18 @@
 require 'spec_helper.rb'
 
-feature 'Testing infrastructure' do
-  scenario 'Can run app and check page content' do
-    visit('/')
-    expect(page).to have_content("Testing infrastructure working!")
-  end
-end
+# feature 'Testing infrastructure' do
+#   scenario 'Can run app and check page content' do
+#     visit('/')
+#     expect(page).to have_content("Testing infrastructure working!")
+#   end
+# end
 
 feature '2 players start game' do
   scenario 'Can add two player names to a form' do
     visit('/')
-    fill_in :player1_name, with: 'Richard'
-    fill_in :player2_name, with: 'Josh'
+    fill_in :player_1, with: 'Joe'
+    fill_in :player_2, with: 'Josh'
     click_button 'Submit'
-    expect(page).to have_content("Richard vs Josh")
+    expect(page).to have_content("Joe vs Josh")
   end
 end
