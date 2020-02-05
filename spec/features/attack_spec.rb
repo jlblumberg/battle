4fa7a3e797a5f443290cab3fe82177c1defe604c
Josh Blumberg <jlblumberg@gmail.com>
2020-02-05 16:21:44 +0000

    Refactor attack button

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 feature 'Can attack' do
   scenario 'can attack and get confirmation' do
     sign_in_and_play
-    click_button 'Attack player 2'
+    click_button 'Attack Josh'
     expect(page).to have_content("Joe attacked Josh")
   end
 end
