e2b532033920f3782aeb2112594655fac96fad21
Joe Ellingworth <joeellingworth@gmail.com>
2020-02-05 17:14:41 +0000

    added unit test for deduct hp

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,14 @@
 
 class Player
-  attr_reader :name
+  attr_reader :name, :hp
   
   def initialize(name)
     @name = name
+    @hp = 100
+  end 
+
+  def deduct_hp(amount)
+    @hp -= amount
   end 
 
 end 
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,8 +1,15 @@
 require 'spec_helper'
 describe Player do 
+  subject(:player) { described_class.new("Joe") }
   describe '#name' do
     it 'should return the name of the player' do 
-      expect(Player.new("Joe").name).to eq ("Joe")
+      expect(player.name).to eq ("Joe")
+    end 
+  end 
+
+  describe '#deduct_hp' do 
+    it 'should deduct from hp' do 
+      expect { player.deduct_hp(10) }.to change{player.hp}.by(-10)
     end 
   end 
 end 
