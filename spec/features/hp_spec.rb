4dd0bcd7e45317c7f9838da6776e74b72175a663
Josh Blumberg <jlblumberg@gmail.com>
2020-02-07 11:05:07 +0000

    Test HP shown for both players on start

---
+++ spec/features/hp_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+feature 'View HP' do
+
+  scenario "see player one's HP" do
+    sign_in_and_play
+    expect(page).to have_content("Joe HP:60")
+  end
+
+  scenario "see player two's HP" do
+    sign_in_and_play
+    expect(page).to have_content("Josh HP:60")
+  end
+
+ends
--- spec/features/starting_health_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-feature 'Can view other players HP' do
-  scenario 'start of game shows players HP' do
-    sign_in_and_play
-    expect(page).to have_content("Josh HP:60")
-  end
-end
